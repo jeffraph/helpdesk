a1d87850d2425ab22905e3ece3fa17e72dd10983
jeffraph <jeffersonraph@gmail.com>
2022-03-06 10:09:46 -0300

    protegendo aplicacao contra serializacao

--- src/main/java/com/jeffraph/helpdesk/domain/Cliente.java
+++ src/main/java/com/jeffraph/helpdesk/domain/Cliente.java
@@ -5,12 +5,14 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.jeffraph.helpdesk.domain.enums.Perfil;
 
 @Entity
 public class Cliente extends Pessoa {
 	private static final long serialVersionUID = 1L;
 	
+	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Chamado> chamados = new ArrayList<>();
 
