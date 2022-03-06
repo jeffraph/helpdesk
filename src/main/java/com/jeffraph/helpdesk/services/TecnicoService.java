411aa2342058891e37894e77c8f9a7c2de78865d
jeffraph <jeffersonraph@gmail.com>
2022-03-06 11:29:49 -0300

    excecao para objeto nao econtrado

--- src/main/java/com/jeffraph/helpdesk/services/TecnicoService.java
+++ src/main/java/com/jeffraph/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.jeffraph.helpdesk.domain.Tecnico;
 import com.jeffraph.helpdesk.repositories.TecnicoRepository;
+import com.jeffraph.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class TecnicoService {
@@ -16,6 +17,6 @@ public class TecnicoService {
 	
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado! ID: " + id));
 	}
 }
