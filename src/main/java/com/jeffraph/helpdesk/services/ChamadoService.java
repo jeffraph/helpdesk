89cda02ff07c50cb4ff61ad5bbc03646d8489537
Jeffe <jeffersonraph@gmail.com>
2022-04-25 12:41:41 -0300

    update para Chamado implementado

--- src/main/java/com/jeffraph/helpdesk/services/ChamadoService.java
+++ src/main/java/com/jeffraph/helpdesk/services/ChamadoService.java
@@ -11,6 +11,7 @@ import com.jeffraph.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -36,6 +37,13 @@ public class ChamadoService {
     public Chamado create(ChamadoDTO objDTO) {
         return repository.save(newChamado(objDTO));
     }
+
+    public Chamado update(Integer id, ChamadoDTO objDTO) {
+        objDTO.setId(id);
+        Chamado oldObj = findById(id);
+        oldObj = newChamado(objDTO);
+        return repository.save(oldObj);
+    }
     private Chamado newChamado(ChamadoDTO obj) {
         Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
         Cliente cliente = clienteService.findById(obj.getCliente());
@@ -43,6 +51,9 @@ public class ChamadoService {
         Chamado chamado = new Chamado();
         if(obj.getId() != null) {
             chamado.setId(obj.getId());
+        }
+        if(obj.getStatus().equals(2)) {
+            chamado.setDataFechamento(LocalDate.now());
         }
         chamado.setTecnico(tecnico);
         chamado.setCliente(cliente);
@@ -52,4 +63,6 @@ public class ChamadoService {
         chamado.setObservacoes(obj.getObservacoes());
         return chamado;
     }
+
+
 }
