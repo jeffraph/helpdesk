e6276d9a93d4b14b69f95ba16a5e41177423c7f4
Jeffe <jeffersonraph@gmail.com>
2022-04-25 12:19:19 -0300

    create para Chamado

--- src/main/java/com/jeffraph/helpdesk/services/ChamadoService.java
+++ src/main/java/com/jeffraph/helpdesk/services/ChamadoService.java
@@ -1,6 +1,11 @@
 package com.jeffraph.helpdesk.services;
 
 import com.jeffraph.helpdesk.domain.Chamado;
+import com.jeffraph.helpdesk.domain.Cliente;
+import com.jeffraph.helpdesk.domain.Tecnico;
+import com.jeffraph.helpdesk.domain.dtos.ChamadoDTO;
+import com.jeffraph.helpdesk.domain.enums.Prioridade;
+import com.jeffraph.helpdesk.domain.enums.Status;
 import com.jeffraph.helpdesk.repositories.ChamadoRepository;
 import com.jeffraph.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +19,10 @@ public class ChamadoService {
 
     @Autowired
     private ChamadoRepository repository;
+    @Autowired
+    private TecnicoService tecnicoService;
+    @Autowired
+    private ClienteService clienteService;
 
     public Chamado findById(Integer id) {
         Optional<Chamado> obj = repository.findById(id);
@@ -22,5 +31,25 @@ public class ChamadoService {
 
     public List<Chamado> findAll() {
         return repository.findAll();
+    }
+
+    public Chamado create(ChamadoDTO objDTO) {
+        return repository.save(newChamado(objDTO));
+    }
+    private Chamado newChamado(ChamadoDTO obj) {
+        Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+        Cliente cliente = clienteService.findById(obj.getCliente());
+
+        Chamado chamado = new Chamado();
+        if(obj.getId() != null) {
+            chamado.setId(obj.getId());
+        }
+        chamado.setTecnico(tecnico);
+        chamado.setCliente(cliente);
+        chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+        chamado.setStatus(Status.toEnum(obj.getStatus()));
+        chamado.setTitulo(obj.getTitulo());
+        chamado.setObservacoes(obj.getObservacoes());
+        return chamado;
     }
 }
